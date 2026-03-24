319c3cdcbcfb542e12ddfed8b038d78d178ca3dd
aman-rwt <amanrawat4060@gmail.com>
2026-03-24 13:41:58 +0530

    feat: enhance main.go to read and display SBOM data from JSON file

--- main.go
+++ main.go
@@ -3,9 +3,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Depsee - Dependency Scanner")
+	jsonFile, err := os.Open("testdata/sbom.json")
+	if err != nil {
+		fmt.Printf("Error opening JSON file: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer jsonFile.Close()
+
+	var sbom SBOM
+	if err := json.NewDecoder(jsonFile).Decode(&sbom); err != nil {
+		fmt.Printf("Error decoding JSON: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("app - %s:%s \n", sbom.Metadata.Component.Name, sbom.Metadata.Component.Version)
+	fmt.Printf("format - %s:%s \n", sbom.BOMFormat, sbom.SpecVersion)
+	fmt.Printf("packages - found: %d \n", len(sbom.Components))
+	fmt.Println("--------------------------------")
+
+	for _, component := range sbom.Components {
+		fmt.Printf("package -> %s:%s \n", component.Name, component.Version)
+	}
 }
